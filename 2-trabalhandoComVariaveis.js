d79ea162697dbc3f61f00b85cb68a3dc9b7dd498
unknown <marcospsw@gmail.com>
2020-07-26 13:30:22 -0300

    Curso Javascript Primeiros Codigos

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,8 @@
+//Console.log("Trabalhando com variáveis");
+console.log("Trabalhando com variáveis");
+// JS é case sensitive
+
+const idade = 29;
+const nome = "Marcos";
+
+
---
+++ 5-atribuicaoDeVariaveis.js
@@ -0,0 +1,15 @@
+console.log("Trabalhando com atribuição variáveis");
+
+
+const idade = 29;
+const primeroNome = "Marcos";
+const sobreprimeroNome = "Wergles";
+
+//console.log(primeroNome + " " + sobreprimeroNome);
+console.log(primeroNome,sobreprimeroNome);
+
+console.log(`Meu primeroNome é ${primeroNome} ${sobreprimeroNome}`);
+
+const primeroNomeCompleto = primeroNome + sobreprimeroNome;
+
+console.log(primeroNomeCompleto);
